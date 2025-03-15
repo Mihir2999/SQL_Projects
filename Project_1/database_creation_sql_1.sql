9b82907ab853097c816b891f736c63ea405005cc
Mihir2999 <mihir.momaya99@gmail.com>
2025-03-15 14:38:24 -0500

    Added a new files for Project 1

---
+++ Project_1/database_creation_sql_1.sql
@@ -0,0 +1,23 @@
+-- Creating Database for 1st SQL Project - Retail Analysis
+CREATE DATABASE sql_project_1;
+
+
+-- Creating Table retail_sales inside our database sql_project_1
+USE sql_project_1;
+CREATE TABLE retail_sales(
+			transactions_id INT PRIMARY KEY,
+            sale_date DATE,
+            sale_time TIME,
+            customer_id	INT,
+            gender VARCHAR(15),
+            age INT NULL,
+            category VARCHAR(25),	
+            quantity INT NULL,	
+            price_per_unit FLOAT NULL,	
+            cogs	FLOAT NULL,
+            total_sale FLOAT NULL
+            );
+
+
+-- Checking the created table
+SELECT COUNT(*) from retail_sales;
