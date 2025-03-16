49354a75bffdaa23a52fdb2ccc8d87a5fcebb48c
Mihir2999 <mihir.momaya99@gmail.com>
2025-03-15 19:04:42 -0500

    Changes in .sql file

--- Project_1/database_creation_sql_1.sql
+++ Project_1/database_creation_sql_1.sql
@@ -21,8 +21,3 @@ CREATE TABLE retail_sales(
 
 -- Checking the created table
 SELECT COUNT(*) from retail_sales;
-
-
--- GRANT ALL PRIVILEGES ON sql_project_1.* TO 'root'@'localhost' IDENTIFIED BY 'Mymummy1@';
-GRANT ALL PRIVILEGES ON sql_project_1.* TO 'root'@'localhost';
-FLUSH PRIVILEGES;
